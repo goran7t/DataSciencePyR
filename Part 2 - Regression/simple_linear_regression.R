76a4893a48073195bbc34a8e30c0117616909807
goran7t <goran7t@yahoo.com>
2017-12-06 23:51:44 +0100

    Simple linear regression

---
+++ Part 2 - Regression/simple_linear_regression.R
@@ -0,0 +1,24 @@
+# Simple Linear Regression
+
+# Importing the dataset
+dataset = read.csv("Salary_Data.csv")
+# dataset = dataset[, 2:3]
+
+# Splitting the dataset into the Training set and Test set
+#install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Salary, SplitRatio = 2/3)
+traing_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+# Feature Scaling
+# traing_set[,2:3] = scale(traing_set[,2:3])
+# test_set[,2:3] = scale(test_set[,2:3])
+
+# Fitting Simple Linear Regression to the Training set
+regressor = lm(formula = Salary ~ YearsExperience,
+               data = traing_set)
+
+# Predicting the Test set results
+y_pred = predict(regressor, newdata = test_set)
